9d2780ee8092951657507ab3db702d857717f3e8
Vadaska <159787695+Vadaska@users.noreply.github.com>
2026-01-28 12:50:59 +0200

    task-4-finish
    123

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,16 @@
 const form = document.querySelector('.login-form');
 form.addEventListener('submit', event => {
   event.preventDefault();
+  const email = form.elements.email;
+  const password = form.elements.password;
+  const login = {};
+
+  if (email.value.trim() === '' || password.value.trim() === '') {
+    alert('All form fields must be filled in');
+  } else {
+    login['email'] = email.value.trim();
+    login['password'] = password.value.trim();
+    console.log(login);
+    form.reset();
+  }
 });
